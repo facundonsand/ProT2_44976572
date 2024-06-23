0d1d584748cd3c7000e347fdf5557d839d4a31e7
facundonsand <sand.facundo@gmail.com>
2024-06-23 13:32:00 -0300

    Proyecto pauny hecho por Facundo Sand

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto realizado basado en pauny.com.ar
---
+++ app/Views/front/quienes_somos.php
@@ -0,0 +1,30 @@
+  <body class="bg-warning-subtle">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100">
+            <img src="assets\img\hombre-brazos-cruzados.jpg" class="card-img-top" alt="...">
+                 <div class="card-body">
+                    <h5 class="card-title">Juan Perez</h5>
+                    <p class="card-text">Presidente y fundador de la empresa</p>
+            </div>
+        </div>
+    </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="assets\img\adolescente-tranquilo-brazos-cruzados.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Carlos Fernandez</h5>
+        <p class="card-text">Vicepresidente de la empresa</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="assets\img\empresaria-confiada-sonriente-que-presenta-brazos-cruzados.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Romina Gonzalez</h5>
+        <p class="card-text">Secretaria</p>
+      </div>
+    </div>
+  </div>
+</div>
